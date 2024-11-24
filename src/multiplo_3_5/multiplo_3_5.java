2794641931aabb33f1d04618c4d144dc44a031d5
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-24 09:20:10 -0300

    Multiplos de 3 e 5 com sua soma

--- src/multiplo_3_5/multiplo_3_5.java
+++ src/multiplo_3_5/multiplo_3_5.java
@@ -1,5 +1,12 @@
 package multiplo_3_5;
 
 public class multiplo_3_5 {
-
+	public static void main(String[] args) {
+		int i;
+		for (i=1;i<=20;i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				i+=i;
+			}
+		}System.out.printf("A soma dos multiplos de I de 1 até 20 é %d",i);
+	}
 }
